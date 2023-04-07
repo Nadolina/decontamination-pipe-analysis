7f85458cc3e59a46dfee94c38c52afeaee6bf72b
Nadolina <nadibraj@gmail.com>
2023-04-07 11:30:35 -0400

    changes to contaminant analysis

--- contaminant_analysis.R
+++ contaminant_analysis.R
@@ -1,6 +1,3 @@
-
-setwd("/Users/nadolinabrajuka/Documents/ROCKU/decontam_analyses/")
-
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
@@ -10,150 +7,24 @@ library(ggbreak)
 library(ggsci)
 library(svglite)
 
-topIDs <- read.csv(file = "clean_JAN31_top_compiled_IDs_2.csv", header = FALSE)
-dim(topIDs)
-colnames(topIDs) <- c("VGP_ID", "scaffold", "accession", "species", "coverage")
-
-summary(as.factor(topIDs$VGP_ID)); summary(as.factor(topIDs$species))
-
-topIDs$species[which(topIDs$species == "no identity")] <- "almond"
-## these accessions are indeed the same as the other almonds 
-## replacing with almonds 
-topIDs$species[which(topIDs$species == "Shigella flexneri")] <- "Escherichia coli"
-## not shigella, true ID is E coli (E coli has the same coverage % as shigella) so I am substituting E coli
-
-## Filtered out almonds and synthetic constructs b/c they were dominating the counts from the mTurTru1 contaminants
-## The histogram below reflects these filtered results without any additional filtering 
-filter_topIDs <- topIDs %>% 
-  filter(species != "almond") %>% 
-  filter(species != "synthetic construct")
-
-ggplot(data = filter_topIDs, mapping = aes(x = species)) + 
-  geom_histogram(stat = "count") + 
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 60, hjust = 1))
-
-## Going to try to group by broader taxon (i.e./ all Defltia, all Escherichia)
-filter_topIDs$species[str_detect(filter_topIDs$species, pattern = "Delftia")] <- "Delftia"
-filter_topIDs$species[str_detect(filter_topIDs$species, pattern = "Escherichia")] <- "Escherichia"
-
-## Same plot as above but with the Delftia and Escherichia lumped together. 
-ggplot(data = filter_topIDs, mapping = aes(x = species)) + 
-  geom_histogram(stat = "count") + 
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 60, hjust = 1))
-
-identifiers <- read.csv("imputed_FEB3_compiled_IDs.csv", header = FALSE)
-dim(identifiers)
-colnames(identifiers) <- c("VGP_ID", "scaffold", "accession", "species", "coverage")
-summary(as.factor(identifiers$VGP_ID))
-
-almonds_only <- topIDs[which(topIDs$species == 'almond'),]
-dim(almonds_only)
-unique(almonds_only$VGP_ID)
-
-not_almonds_ind<- which(!duplicated(identifiers[which(identifiers$species != 'almond'),c("VGP_ID","scaffold")]))
-not_almonds <- identifiers[not_almonds_ind,]
-dim(not_almonds)
-
-not_almonds <- identifiers[which(identifiers$species != 'almond'),]
-not_almonds_top_IDs_ind<- which(!duplicated(not_almonds[,c("VGP_ID","scaffold")]))
-not_almonds_top_IDs <- not_almonds [not_almonds_top_IDs_ind,]
-dim(not_almonds_top_IDs)
-
-merge_df<- full_join(topIDs, not_almonds_top_IDs, by = c("VGP_ID","scaffold"))
-merge_df$species.x == "almond"
-merge_df$species.y == "almond"
-
-former_almonds <- merge_df[merge_df$species.x == "almond",]
-dim(former_almonds)
-summary(as.factor(former_almonds$VGP_ID))
-summary(as.factor(former_almonds$species.y))
-summary(former_almonds$coverage.x); summary(as.numeric(former_almonds$coverage.y))
-
-ggplot(data = not_almonds_top_IDs, mapping = aes(x = species)) + 
-  geom_histogram(stat = "count") + 
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 60, hjust = 1))
-
-not_almonds_top_IDs$coverage<-round(as.numeric(not_almonds_top_IDs$coverage),0)
-
-not_almonds_top_IDs$species[not_almonds_top_IDs$species == "Staphylococcus.aureus"] <- "Staphylococcus aureus"
-not_almonds_top_IDs$species[not_almonds_top_IDs$species == "no identification"] <- "synthetic construct" ##verified the no identification ones are synthetic constructs
-not_almonds_top_IDs$species[str_detect(not_almonds_top_IDs$species, pattern = "Leptospira")] <- "Leptospira"
-
-## with jitter
-#ggplot(data = not_almonds_top_IDs, mapping = aes(x = VGP_ID, y = species,colour= as.numeric(coverage), size = as.numeric(coverage))) + 
-ggplot(data = not_almonds_top_IDs, mapping = aes(x = VGP_ID, y = species,colour= as.numeric(coverage))) +
-  geom_point() + 
-  geom_jitter(position = "jitter") + 
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
-  labs(colour = "Coverage") +
-  xlab("VGP Identifier") + 
-  ylab("Contaminant") +
-  scale_color_viridis_c(option="rocket", begin = 0.1, end = 0.6, direction = 1) +
-  guides(size = FALSE)
-
-##without jitter
-ggplot(data = not_almonds_top_IDs, mapping = aes(x = VGP_ID, y = species,colour= as.numeric(coverage), size = as.numeric(coverage))) + 
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
-  labs(colour = "Coverage") +
-  xlab("VGP Identifier") + 
-  ylab("Contaminant") +
-  scale_color_viridis_c(option="rocket", begin = 0.1, end = 0.6, direction = 1, alpha = 0.5) +
-  guides(size = FALSE)
-
-ggplot(data = not_almonds_top_IDs, mapping = aes (x = VGP_ID, y = as.numeric(coverage), colour = as.factor(species))) +
-  geom_count() +
-  scale_y_break(breaks = c(90,94), scales = 3)
-##same as above but with jitter instead of count
-ggplot(data = not_almonds_top_IDs, mapping = aes (x = VGP_ID, y = as.numeric(coverage), colour = as.factor(species))) +
-  geom_point(position = "jitter") +
-  scale_y_break(breaks = c(90,94), scales = 3) +
-  theme_bw() +
-  labs(colour = "Contaminant") +
-  ylab("Coverage") +
-  xlab("VGP Identifier") +
-  theme(axis.text.x = element_text(angle = 45, hjust =1)) +
-  guides(size = FALSE) +
-  scale_color_tron()
-
 ## contaminant analysis but with the sizes of all the contaminants 
 
-comp_IDs <- read.csv(file = "compiled_IDs_MAR3.csv", header = FALSE)
+comp_IDs <- read.csv(file = "/Users/nadolinabrajuka/Documents/ROCKU/decontam_analyses/compiled_IDs_MAR3.csv", 
+                     header = FALSE)
 head(comp_IDs)
-
 colnames(comp_IDs) <- c("VGP_ID","scaffold","acc_num", "species", "coverage","size")
 
 summary(comp_IDs)
 summary(as.factor(comp_IDs$species))
 
-#top_IDs_2 <- read.csv(file="top_ID_contam_lengths.csv", header = FALSE)
-#head(top_IDs_2)
-#colnames(top_IDs_2) <- c("VGP_ID","scaffold","acc_num","species","coverage","size")
-
-#summary(top_IDs_2); dim(top_IDs_2)
-
-#summary(as.factor(top_IDs_2$species))
-
-# almonds_only <- top_IDs_2[which(top_IDs_2$species == 'almond'),]
-# dim(almonds_only)
-# unique(almonds_only$VGP_ID) #observed in seven different assemblies 
-
-not_almonds <- comp_IDs[which(comp_IDs$species != 'almond'),] ## subsetting rows where almond isn't the identified species
+## subsetting rows where the contaminant species is not labeled 'almond' or 'no identification'
+not_almonds <- comp_IDs[which(comp_IDs$species != 'almond'),] 
 not_noID <- not_almonds[which(not_almonds$species != 'no identification'),]
 dim(not_noID)
+
 filt_topID_ind <- which(!duplicated(not_noID[,c("VGP_ID","scaffold")])) ##isolating the index for the top non-almond species ID for each unique VGP_ID-scaffold pair 
 filt_topIDs <- not_noID[filt_topID_ind,] ## top ID for each unique VGP_ID/scaffold pair that isn't almond or "no identification"
-dim(filt_topIDs)
-#dim(not_almonds_topIDs);summary(as.factor(not_almonds_topIDs$species))
-
-# merge_df <- full_join(top_IDs_2, filt_topIDs, by.x = c("VGP_ID","scaffold"))
-# dim(merge_df); head (merge_df) ## SOMETHING IS WRONG WITH THIS MERGE DO NOT USE IT 
-# 
-# which(top_IDs_2$VGP_ID != not_almonds_top_IDs$VGP_ID)
+dim(filt_topID)
 
 top_IDs_3 <- comp_IDs[which(!duplicated(comp_IDs[,c("VGP_ID","scaffold")])),] ##CORRECTLY ISOLATED THE TOP ID FOR EACH UNIQUE ID-SCAFFOLD PAIR, USE THIS ONE 
 ## this would include almonds 
@@ -164,12 +35,6 @@ top_IDs_3$scaffold == filt_topIDs$scaffold; top_IDs_3$VGP_ID == filt_topIDs$VGP_
 ## merging the original top IDs and the filtered top IDs for comparison 
 merge_df <- full_join(top_IDs_3, filt_topIDs, by = c("VGP_ID","scaffold"))
 dim(merge_df); head(merge_df);tail(merge_df)
-
-#checking to make sure not more almonds/"no identifications" are present 
-sum(merge_df$species.x == "almond")
-sum(merge_df$species.y == "almond")
-sum(merge_df$species.x == "no identification")
-sum(merge_df$species.y == "no identification")
 
 former_almonds <- merge_df[merge_df$species.x == "almond",] ##isolating rows of the merged table where almond was the initial ID 
 dim(former_almonds)
